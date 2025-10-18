08a406b4e2e7f083787a2ae6f3ff6a74b07a1cf7
Hazelgrover <135610834+Hazelgrover@users.noreply.github.com>
2025-10-19 02:46:53 +0700

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from django.http import Http404
 from .models import Post, Category
+
 
 def index(request):
     posts = Post.objects.filter(
@@ -10,6 +10,7 @@ def index(request):
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')[:5]
     return render(request, 'blog/index.html', {'post_list': posts})
+
 
 def post_detail(request, post_id):
     post = get_object_or_404(
@@ -22,6 +23,7 @@ def post_detail(request, post_id):
     )
     return render(request, 'blog/detail.html', {'post': post})
 
+
 def category_posts(request, category_slug):
     category = get_object_or_404(
         Category,
@@ -33,4 +35,5 @@ def category_posts(request, category_slug):
         is_published=True,
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')
-    return render(request, 'blog/category.html', {'category': category, 'post_list': posts})
+    return render(request, 'blog/category.html',
+                   {'category': category, 'post_list': posts})
